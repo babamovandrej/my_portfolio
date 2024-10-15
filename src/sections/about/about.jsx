9a54fe65346e4cf3bd491d40d4013d81eed72dc8
babamovandrej <andrejbabamov@gmail.com>
2024-10-15 15:53:33 +0200

    Fixed about page

--- src/sections/about/about.jsx
+++ src/sections/about/about.jsx
@@ -1,57 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./about.css";
 
 function About() {
-    const aboutRef = useRef(null);
-
-    useEffect(() => {
-        const handleIntersection = (entries) => {
-            entries.forEach(entry => {
-                const paragraphs = entry.target.querySelectorAll("p");
-                if (entry.isIntersecting) {
-                    addAnimationClasses(paragraphs);
-                } else {
-                    removeAnimationClasses(paragraphs);
-                }
-            });
-        };
-
-        const observer = new IntersectionObserver(handleIntersection, {
-            threshold: 0.5
-        });
-
-        if (aboutRef.current) {
-            observer.observe(aboutRef.current);
-        }
-
-        return () => {
-            if (aboutRef.current) {
-                observer.unobserve(aboutRef.current);
-            }
-        };
-    }, []);
-
-    const addAnimationClasses = (paragraphs) => {
-        paragraphs.forEach((paragraph, index) => {
-            paragraph.classList.add(index === 0 ? "animateStripfromLeft" : "animateStripfromRight");
-        });
-    };
-
-    const removeAnimationClasses = (paragraphs) => {
-        paragraphs.forEach((paragraph, index) => {
-            paragraph.classList.remove(index === 0 ? "animateStripfromLeft" : "animateStripfromRight");
-        });
-    };
-
     return (
-        <div className='about-container' ref={aboutRef}>
+        <div className="about-container">
             <p>
-                Hello! I'm <span className='highlight'>Andrej Babamov</span>, a Full Stack Developer
-            </p>
-            <p>
-                <span className='new-line'>
-                    currently based in <span className='highlight'>Skopje, Macedonia</span>.
-                </span>
+                Hey! I'm <span className="highlight">Andrej Babamov</span>, a Full Stack Developer
+                <br />
+                currently based in <span className="highlight">Skopje, Macedonia</span>.
             </p>
         </div>
     );
