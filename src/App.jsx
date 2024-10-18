a3d55ceb678c25bb9a00480ba93cd8a0f9afdbf1
babamovandrej <andrejbabamov@gmail.com>
2024-10-18 12:56:23 +0200

    Add custom cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Background from "./components/background/background";
+import Cursor from "./components/cursor/cursor";
 import SideMenu from "./components/side-menu/side-menu";
 import SocialsMenu from "./components/socials-menu/socials-menu";
 import Sections from "./sections/sections";
@@ -11,6 +12,7 @@ function App() {
 			<SideMenu />
 			<SocialsMenu />
 			<Sections />
+			<Cursor />
 		</>
 	);
 }
---
+++ src/components/cursor/cursor.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import './cursor.css';
+
+const CustomCursor = () => {
+    const cursorRef = useRef(null);
+
+    useEffect(() => {
+        const customCursor = cursorRef.current;
+
+        const updateCursorPosition = (event) => {
+            customCursor.style.top = `${event.clientY}px`;
+            customCursor.style.left = `${event.clientX}px`;
+        };
+
+        const handleMouseMove = (event) => {
+            updateCursorPosition(event);
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    return <div className="cursor" ref={cursorRef}></div>;
+};
+
+export default CustomCursor;
